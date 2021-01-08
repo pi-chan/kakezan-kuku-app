ee442aa08b296ec041e0bc323a3ae6d338d550c7
pi-chan <hiromasa.ohno.1007@gmail.com>
2021-01-08 19:38:35 +0900

    no scalable

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     title: 'かけ算',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
